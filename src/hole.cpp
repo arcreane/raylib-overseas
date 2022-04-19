dd73e406b94e06b2e2c42e1e3716c2265a535583
cytech <ruben.alk@hotmail.fr>
2022-04-19 23:23:04 +0200

    Reverting commit "Creating class file for hole"

--- src/hole.cpp
+++
@@ -1,5 +0,0 @@
-/* Inclusion du header de la classe Hole */
-#include "Hole.h"
-
-/* Implémentation du constructeur de la classe Ball */
-Hole::Hole(Vector2f p_pos, SDL_Texture* p_tex) : Entity(p_pos, p_tex) {}
