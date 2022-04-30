d77153bd55d7bb65dd3d9afc51e580c2e814a628
RayanK95 <101478587+RayanK95@users.noreply.github.com>
2022-04-30 19:41:45 +0200

    Add files via upload
    Added new level, changed logo position

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,11 @@
 #include <SDL2/SDL_mixer.h>
 #include <iostream>
 #include <vector>
-#include <fstream>
-#include <cstdlib>
 
 /* Headers dezs différentes classes */
 #include "RenderWindow.h"
 #include "Entity.h"
-#include "Whiteball.h"	
-#include "Redball.h"	
-#include "Blueball.h"	
+#include "Ball.h"	
 #include "Tile.h"
 #include "Hole.h"
 
@@ -34,23 +30,30 @@ bool SDLinit = init();
 RenderWindow window("Overseas-Golf", 320, 480);
 
 /* Rendu des textures */
-SDL_Texture* ballTextureWhite = window.loadTexture("resources/gfx/ball.png");
-SDL_Texture* ballTextureBlue = window.loadTexture("resources/gfx/blueball.png");
-SDL_Texture* ballTextureRed = window.loadTexture("resources/gfx/redball.png");
+SDL_Texture* ballTexture = window.loadTexture("resources/gfx/ball.png");
 SDL_Texture* holeTexture = window.loadTexture("resources/gfx/hole.png");
 SDL_Texture* pointTexture = window.loadTexture("resources/gfx/point.png");
 SDL_Texture* tileDarkTexture32 = window.loadTexture("resources/gfx/tile32_dark.png");
 SDL_Texture* tileDarkTexture64 = window.loadTexture("resources/gfx/tile64_dark.png");
 SDL_Texture* tileLightTexture32 = window.loadTexture("resources/gfx/tile32_light.png");
 SDL_Texture* tileLightTexture64 = window.loadTexture("resources/gfx/tile64_light.png");
+SDL_Texture* tileFlakeTexture32 = window.loadTexture("resources/gfx/tile32_flake.png");
+SDL_Texture* tileFlakeGreyTexture32 = window.loadTexture("resources/gfx/tile32_flakegrey.png");
+SDL_Texture* tileFlakeTexture64 = window.loadTexture("resources/gfx/tile64_flake.png");
+SDL_Texture* tileSnowmanTexture32 = window.loadTexture("resources/gfx/tile32_snowman.png");
+SDL_Texture* tileSnowmanGreyTexture32 = window.loadTexture("resources/gfx/tile32_snowmangrey.png");
+SDL_Texture* tileSnowmanTexture64 = window.loadTexture("resources/gfx/tile64_snowman.png");
+SDL_Texture* tileTreeTexture32 = window.loadTexture("resources/gfx/tile32_tree.png");
+SDL_Texture* tileTreeGreyTexture32 = window.loadTexture("resources/gfx/tile32_treegrey.png");
 SDL_Texture* ballShadowTexture = window.loadTexture("resources/gfx/ball_shadow.png");
 SDL_Texture* bgTexture = window.loadTexture("resources/gfx/bg.png");
+SDL_Texture* bgTextureSnow = window.loadTexture("resources/gfx/bgSnow.png");
 SDL_Texture* uiBgTexture = window.loadTexture("resources/gfx/UI_bg.png");
 SDL_Texture* levelTextBgTexture = window.loadTexture("resources/gfx/levelText_bg.png");
 SDL_Texture* powerMeterTexture_FG = window.loadTexture("resources/gfx/powermeter_fg.png");
 SDL_Texture* powerMeterTexture_BG = window.loadTexture("resources/gfx/powermeter_bg.png");
 SDL_Texture* powerMeterTexture_overlay = window.loadTexture("resources/gfx/powermeter_overlay.png");
-
+SDL_Texture* logoTexture = window.loadTexture("resources/gfx/logo.png");
 SDL_Texture* click2start = window.loadTexture("resources/gfx/click2start.png");
 SDL_Texture* endscreenOverlayTexture = window.loadTexture("resources/gfx/end.png");
 SDL_Texture* splashBgTexture = window.loadTexture("resources/gfx/splashbg.png");
@@ -66,18 +69,7 @@ TTF_Font* font32 = TTF_OpenFont("resources/font/font.ttf", 32);
 TTF_Font* font48 = TTF_OpenFont("resources/font/font.ttf", 48);
 TTF_Font* font24 = TTF_OpenFont("resources/font/font.ttf", 24);
 
-/* 
---- Définition de  la balle utilisée ---
-
-	Whiteball = balle classique
-	Redball = balle qui s'écrase sur les murs
- 	Blueball = balle qui glisse plus longtemps
-*/
-
-Whiteball ball = Whiteball(Vector2f(0, 0), ballTextureWhite, pointTexture, powerMeterTexture_FG, powerMeterTexture_BG, 0);
-//Redball ball = Redball(Vector2f(0, 0), ballTextureRed, pointTexture, powerMeterTexture_FG, powerMeterTexture_BG, 0);
-//Blueball ball = Blueball(Vector2f(0, 0), ballTextureBlue, pointTexture, powerMeterTexture_FG, powerMeterTexture_BG, 0);
-
+Ball ball = Ball(Vector2f(0, 0), ballTexture, pointTexture, powerMeterTexture_FG, powerMeterTexture_BG, 0);
 Hole hole = Hole(Vector2f(0, 0), holeTexture);
 
 std::vector<Tile> loadTiles(int level)
@@ -107,15 +99,28 @@ std::vector<Tile> loadTiles(int level)
 		break;
 
 		case 4:
-			temp.push_back(Tile(Vector2f(32*3, 32*1), tileDarkTexture32));
-			temp.push_back(Tile(Vector2f(32*1, 32*3), tileDarkTexture32));
-			temp.push_back(Tile(Vector2f(32*5, 32*3), tileDarkTexture32));
-			temp.push_back(Tile(Vector2f(32*3, 32*5), tileDarkTexture32));
-			temp.push_back(Tile(Vector2f(32*7, 32*5), tileDarkTexture32));
-			temp.push_back(Tile(Vector2f(32*7, 32*10), tileDarkTexture32));
-			temp.push_back(Tile(Vector2f(32*3, 32*10), tileDarkTexture32));
-			temp.push_back(Tile(Vector2f(32*5, 32*12), tileDarkTexture32));
-			temp.push_back(Tile(Vector2f(32*7, 32*10), tileDarkTexture32));
+			temp.push_back(Tile(Vector2f(32*3, 32*1), tileSnowmanTexture32)); 
+			temp.push_back(Tile(Vector2f(32*1, 32*3), tileFlakeTexture32)); 
+			temp.push_back(Tile(Vector2f(32*5, 32*3), tileFlakeTexture32));
+			temp.push_back(Tile(Vector2f(32*3, 32*5), tileSnowmanTexture32));
+			temp.push_back(Tile(Vector2f(32*7, 32*5), tileFlakeTexture32));
+			temp.push_back(Tile(Vector2f(32*7, 32*10), tileSnowmanTexture32));
+			temp.push_back(Tile(Vector2f(32*3, 32*10), tileSnowmanGreyTexture32));
+			temp.push_back(Tile(Vector2f(32*5, 32*12), tileFlakeGreyTexture32)); 
+			temp.push_back(Tile(Vector2f(32*7, 32*10), tileFlakeGreyTexture32)); 
+			temp.push_back(Tile(Vector2f(32*2, 32*7), tileSnowmanGreyTexture32)); 
+			temp.push_back(Tile(Vector2f(32*2, 32*9), tileFlakeGreyTexture32)); 
+			temp.push_back(Tile(Vector2f(32*0, 32*9), tileTreeGreyTexture32)); 
+			temp.push_back(Tile(Vector2f(32*5, 32*8), tileTreeGreyTexture32));
+			temp.push_back(Tile(Vector2f(32*4, 32*9), tileTreeGreyTexture32));  
+			temp.push_back(Tile(Vector2f(32*5, 32*10), tileTreeGreyTexture32)); 
+			temp.push_back(Tile(Vector2f(32*6, 32*9), tileTreeGreyTexture32)); 
+			temp.push_back(Tile(Vector2f(32*9, 32*10), tileSnowmanGreyTexture32));
+			temp.push_back(Tile(Vector2f(32*9, 32*6), tileSnowmanGreyTexture32));
+			temp.push_back(Tile(Vector2f(32*1, 32*14), tileSnowmanGreyTexture32));
+			temp.push_back(Tile(Vector2f(32*7, 32*1), tileTreeTexture32));
+			temp.push_back(Tile(Vector2f(32*8, 32*3), tileTreeGreyTexture32));
+
 		break;
 	}
 	return temp;
@@ -145,9 +150,6 @@ void loadLevel(int level)
 	if (level > 4)
 	{
 		state = 2;
-		std::ofstream ofs ("scores.txt", std::ios::app);
-		ofs << __DATE__ << " - " << __TIME__ << " : " << ball.getStrokes() << " coups.\n";
-		ofs.close();
 		return;
 	}
 	
@@ -192,7 +194,7 @@ const char* getStrokeText()
 
 const char* getLevelText()
 {
-	std::string s = std::to_string(level+1);
+	std::string s = std::to_string(level);
 	s = "TROU: " + s;
 	return s.c_str();
 }
@@ -243,7 +245,14 @@ void update()
 void graphics()
 {
 	window.clear();
-	window.render(0, 0, bgTexture);
+	if (level==4)
+	{
+		window.render(0, 0, bgTextureSnow);
+	}
+	else
+	{
+		window.render(0, 0, bgTexture);
+	}
 	window.render(hole);
 	if (!ball.isWin())
 	{
@@ -279,7 +288,7 @@ void graphics()
 	else
 	{
 		window.render(0, 0, endscreenOverlayTexture);
-		window.renderCenter(0, 3 - 32, "YOU COMPLETED THE COURSE!", font48, black);
+		window.renderCenter(0, 3 -32, "YOU COMPLETED THE COURSE!", font48, black);
 		window.renderCenter(0, -32, "YOU COMPLETED THE COURSE!", font48, white);
 		window.renderCenter(0, 3 + 32, getStrokeText(), font32, black);
 		window.renderCenter(0, 32, getStrokeText(), font32, white);
@@ -310,8 +319,8 @@ void titleScreen()
 		window.clear();
 		window.render(0, 0, bgTexture);
 		window.render(0, 0, splashBgTexture);
-		window.renderCenter(-150, 0 + 3, "OVERSEAS.", font32, black);
-		window.renderCenter(-150, 0, "OVERSEAS.", font32, white);
+		window.renderCenter(0, 0 , "POLYMARS", font32, black);
+		window.renderCenter(0, 0, "POLYMARS", font32, white);
 		window.display();
 	}
 	else
@@ -344,10 +353,10 @@ void titleScreen()
 		}
 		window.clear();
 		window.render(0, 0, bgTexture);
-		//window.render(320 - 160, 240 - 100 - 50 + 4*SDL_sin(SDL_GetTicks()*(3.14/1500)), logoTexture);
+		window.render(160 - 160, 240 - 100 - 50 + 4*SDL_sin(SDL_GetTicks()*(3.14/1500)), logoTexture);
 		window.render(0, 0, click2start);
-		window.renderCenter(-150, 240 - 48 + 3 - 16*5, "LEFT CLICK TO START", font24, black);
-		window.renderCenter(-150, 240 - 48 - 16*5, "LEFT CLICK TO START", font24, white);
+		window.renderCenter(0, 240 - 48 + 3 - 16*5, "LEFT CLICK TO START", font32, black);
+		window.renderCenter(0, 240 - 48 - 16*5, "LEFT CLICK TO START", font32, white);
 		window.display();
 	}
 }
